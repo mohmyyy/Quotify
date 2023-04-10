0714fd42af9ea21463e6b3e8fb98017a7c0a8354
mohmyyy <iammohmy@gmail.com>
2023-04-10 07:06:54 +0530

    deploting

--- src/Components/lib/api.js
+++ src/Components/lib/api.js
@@ -1,4 +1,4 @@
-const FIREBASE_DOMAIN = 'https://building-quotify-default-rtdb.firebaseio.com/';
+const FIREBASE_DOMAIN = 'https://qu0tify-default-rtdb.firebaseio.com/';
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
